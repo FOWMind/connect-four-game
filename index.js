d2f0237da6d0b41b4f6c1b2740e86ee8ae277d1f
FOWMind <alwl1@hotmail.com>
2022-12-06 04:55:51 -0300

    [Add] Favicon

--- index.js
+++ index.js
@@ -6,8 +6,9 @@ import EventManager from "./src/modules/EventManager.js";
  * - [x] Add icons to the menu buttons.
  * - [X] Change game box color when turn changes.
  * - [x] Show the game rules when user clicks Game rules button.
- * - Add favicon.
+ * - [x] Add favicon.
  * - When the turn time ends, the game must show a Lose game box.
+ * - Add a confirm dialog when press restart if the game is not over.
  * - Find a bug where the four-in-row check doesn't work correctly.
  * and the game continue even with a four-in-row (it occurs when I try to get a tie).
  * - End with mobile/tablet/desktop design.
